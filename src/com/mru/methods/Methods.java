19238c3cdcca488a84d6d7ee4b46ff406056633c
Aarnesh18 <rsaiprabhaaarnesh@gmail.com>
2025-01-08 19:56:05 +0530

    assignment day 3

---
+++ src/com/mru/methods/Methods.java
@@ -0,0 +1,15 @@
+package com.mru.methods;
+public class Methods {
+
+
+	public static void main(String[] args) {
+		Mru ob = new Mru();
+		Mru.dsa1();
+		Mru.dsa2("MRU");
+		String r=Mru.dsa3();
+		System.out.println(r);
+		String re =Mru.dsa4("CodeChef");
+		System.out.println(re);
+	}
+
+}
---
+++ src/com/mru/methods/Mru.java
@@ -0,0 +1,22 @@
+package com.mru.methods;
+
+public class Mru {
+	
+		
+	
+		public static void dsa1() {
+			System.out.println("Welcome to Dsa1 ");
+		}
+		
+		public static void dsa2(String name) {
+			System.out.println(name);
+		}
+		
+		public static String dsa3() {
+			return "welcome to leetcode";
+		}
+		
+		public static  String dsa4(String name) {
+			return name;
+		}
+}
